375aa70415e9c77f180b0b20fa9de547b78c3b4f
michae1bo <159921466+michae1bo@users.noreply.github.com>
2025-03-03 11:48:01 +0000

    feat: implement crack_sha1_hash function

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,24 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    if use_salts:
+        salts = open('known-salts.txt', 'rt')
+        salt_list = salts.read().splitlines()
+        salts.close()
+    known_passwords = open('top-10000-passwords.txt', 'rt')
+    password_list = known_passwords.read().splitlines()
+    known_passwords.close()
+    for password in password_list:
+        if use_salts:
+            for salt in salt_list:
+                salted_password = salt + password
+                if hash == hashlib.sha1(salted_password.encode()).hexdigest():
+                    return password
+                salted_password = password + salt
+                if hash == hashlib.sha1(salted_password.encode()).hexdigest():
+                    return password
+        elif hash == hashlib.sha1(password.encode()).hexdigest():
+            return password
+        
+    
+    return "PASSWORD NOT IN DATABASE"
